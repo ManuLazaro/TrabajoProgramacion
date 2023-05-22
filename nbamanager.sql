0943e8c7d708e9dcf28c423c3257ff519e646bbc
MarioRodriguezCordero <mario.rodcor@educa.jcyl.es>
2023-05-22 12:30:40 +0200

    Update nbamanager.sql

--- nbamanager.sql
+++ nbamanager.sql
@@ -1,13 +1,16 @@
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: nbamanager
+-- Host: 127.0.0.1    Database: nbamanager
 -- ------------------------------------------------------
 -- Server version	8.0.30
 
+DROP DATABASE IF EXISTS nbamanager;
+CREATE DATABASE nbamanager;
+USE nbamanager;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -56,7 +59,7 @@ CREATE TABLE `jugadores` (
   `Posicion` varchar(20) DEFAULT NULL,
   `Tiro` int DEFAULT NULL,
   `Defensa` int DEFAULT NULL,
-  `Destreza` int DEFAULT NULL,
+  `Destreza` varchar(10) DEFAULT NULL,
   `Coste` int DEFAULT NULL,
   PRIMARY KEY (`idJugadores`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
@@ -68,6 +71,7 @@ CREATE TABLE `jugadores` (
 
 LOCK TABLES `jugadores` WRITE;
 /*!40000 ALTER TABLE `jugadores` DISABLE KEYS */;
+INSERT INTO `jugadores` VALUES (1,'Lebron James','Alero',87,13,'Tirar',35),(2,'Stephen Curry','Base',68,33,'Driblar',34),(3,'Giannis Antetokounmpo','Ala-Pivot',70,30,'Driblar',38),(4,'Nikola Jokić','Pivot',84,16,'Tirar',22),(5,'Anthony Davis','Ala-Pivot',64,36,'Tirar',34),(6,'Luka Dončić','Alero',68,32,'Driblar',36),(7,'Devin Booker','Escolta',20,80,'Defender',40),(8,'Damian Lillard','Base',24,76,'Defender',47),(9,'Jimmy Butler','Escolta',34,66,'Defender',29),(10,'Joel Embiid','Pivot',78,22,'Driblar',32);
 /*!40000 ALTER TABLE `jugadores` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,6 +96,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Pepe','Pepito'),(2,'Carlos','1234');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -104,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-19 11:00:17
+-- Dump completed on 2023-05-22 12:28:25
